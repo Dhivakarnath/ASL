be400a490a7d26ebc76ed423374443cb70284e91
Dhivakarnath <126226293+Dhivakarnath@users.noreply.github.com>
2025-01-24 14:16:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import mediapipe as mp
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import io
-
+import tf_keras as k3
 # Initialize FastAPI app
 app = FastAPI()
 
@@ -17,7 +17,7 @@ hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
 # Load the gesture recognizer model
-model = load_model(r'D:\Sign_language_mediapipe\mp_hand_gesture')
+model = k3.models.load_model(r'D:\Sign_language_mediapipe\mp_hand_gesture')
 
 # Load class names
 with open('gesture.names', 'r') as f:
